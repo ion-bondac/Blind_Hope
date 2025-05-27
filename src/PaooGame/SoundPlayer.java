88102554ce2cecab3545a1cf86fdd6bb62e9fa35
davidb1010 <david-nicusor.buraga@student.tuiasi.ro>
2025-05-27 23:01:38 +0300

    Added music for menu and levels

--- src/PaooGame/SoundPlayer.java
+++ src/PaooGame/SoundPlayer.java
@@ -20,4 +20,27 @@ public class SoundPlayer {
             e.printStackTrace();
         }
     }
+
+    private static Clip backgroundClip;
+    public static void playLoopingSound(String fileName) {
+        try {
+            URL soundURL = SoundPlayer.class.getResource(fileName);
+            if (soundURL == null) {
+                System.err.println("Could not find sound file: " + fileName);
+                return;
+            }
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(soundURL);
+            backgroundClip = AudioSystem.getClip();
+            backgroundClip.open(audioStream);
+            backgroundClip.loop(Clip.LOOP_CONTINUOUSLY); // Loop forever
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void stopBackgroundSound() {
+        if (backgroundClip != null && backgroundClip.isRunning()) {
+            backgroundClip.stop();
+            backgroundClip.close();
+        }
+    }
 }
